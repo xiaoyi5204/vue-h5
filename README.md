bb5e6e40afa853ecf91cf90ee956e6ace62d8031
GuoLi <54439214+xiaoyi5204@users.noreply.github.com>
2024-07-24 11:55:50 +0800

    部分修改

--- README.md
+++ README.md
@@ -1124,8 +1124,3 @@ Vscode setting.json 设置
 
 [▲ 回顶部](#top)
 
-# 鸣谢 ​
-
-[vue-cli4-config](https://github.com/staven630/vue-cli4-config)  
-[vue-element-admin](https://github.com/PanJiaChen/vue-element-admin)
-
--- src/views/home/about.vue
+++ src/views/home/about.vue
@@ -4,19 +4,9 @@
       <div class="list">
         <div class="logo"></div>
         <div class="demo-home__title">VUE H5开发模板</div>
-        <div class="item">
-          项目地址:
-          <a href="https://github.com/sunniejs/vue-h5-template">https://github.com/sunniejs/vue-h5-template</a>
-        </div>
-        <div class="item">项目作者: sunnie</div>
         <div class="item"></div>
-        <div class="wechat">
-          <img :src="this.wechat" alt="" />
-        </div>
-        <div class="item">关注公众号：回复“加群”即可加 前端仙女群</div>
         <div class="item">
-          {{ userName }}
-          <van-button v-if="userName == ''" type="warning" size="small" @click="doDispatch">快点我~</van-button>
+          <van-button size="small" @click="doDispatch">点我点我~</van-button>
         </div>
       </div>
     </div>
